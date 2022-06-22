fef07cb876432a1aa0146d0e38c7f07c3bd68909
В. Александр <spbave@mail.ru>
2022-06-22 14:21:39 +0300

    Описана логика conftest и написан тест для проверки кнопки без assert

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
 
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_should_be_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
