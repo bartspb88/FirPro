e64f5ce429f3de5c6554cd6552d217cf50d12ad2
В. Александр <spbave@mail.ru>
2022-06-22 15:08:56 +0300

    Добавлен assert

--- test_items.py
+++ test_items.py
@@ -7,4 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_should_be_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(10)
-    browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    check_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    check_button_text = check_button.text
+    assert check_button, "Кнопки нет на странице"
